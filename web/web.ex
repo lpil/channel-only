b54cdf239e3a3277e116055d9a1570a669c91454
Louis Pilfold <louis@lpil.uk>
2016-03-30 10:58:14 +0100

    Strip out non-channel code

--- web/web.ex
+++ web/web.ex
@@ -5,8 +5,7 @@ defmodule ChannelOnly.Web do
 
   This can be used in your application as:
 
-      use ChannelOnly.Web, :controller
-      use ChannelOnly.Web, :view
+      use ChannelOnly.Web, :channel
 
   The definitions below will be executed for every view,
   controller, etc, so keep them short and clean, focused
@@ -15,40 +14,6 @@ defmodule ChannelOnly.Web do
   Do NOT define functions inside the quoted expressions
   below.
   """
-
-  def model do
-    quote do
-      # Define common model functionality
-    end
-  end
-
-  def controller do
-    quote do
-      use Phoenix.Controller
-
-      import ChannelOnly.Router.Helpers
-      import ChannelOnly.Gettext
-    end
-  end
-
-  def view do
-    quote do
-      use Phoenix.View, root: "web/templates"
-
-      # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
-
-      import ChannelOnly.Router.Helpers
-      import ChannelOnly.ErrorHelpers
-      import ChannelOnly.Gettext
-    end
-  end
-
-  def router do
-    quote do
-      use Phoenix.Router
-    end
-  end
 
   def channel do
     quote do
